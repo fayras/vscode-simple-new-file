5b52b005dc12b786bd1e324aec9292489f5ce818
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2017-11-03 19:36:11 +0100

    Basic functionality: - Create Files (includung directories) - Create directory by ending with '/'

---
+++ src/FileManager.ts
@@ -0,0 +1,52 @@
+import { Uri, workspace } from 'vscode';
+import * as mkdirp from 'mkdirp';
+import * as path from 'path';
+import * as fs from 'fs';
+
+export default class FileManager {
+  private base: Uri;
+  private root: Uri = workspace.workspaceFolders[0].uri;
+
+  constructor(base: Uri) {
+    this.base = Uri.file(path.dirname(base.fsPath));
+  }
+
+  create(pPath: string): Promise<string> {
+    let base = this.getBase(pPath);
+
+    return new Promise((resolve, reject) => {
+      let filePath = path.join(base, pPath);
+      fs.exists(filePath, (exists: boolean) => {
+        if (exists) {
+          resolve(filePath);
+        } else {
+          let dirPath = FileManager.isDir(filePath) ? filePath : path.dirname(filePath);
+          mkdirp.sync(dirPath);
+          if(!FileManager.isDir(filePath)) {
+            fs.writeFile(filePath, '', (err: NodeJS.ErrnoException) => {
+              if(err) {
+                reject(err.message);
+              } else {
+                resolve(filePath);
+              }
+            });
+          } else {
+            resolve(filePath);
+          }
+        }
+      });
+    });
+  }
+
+  getBase(path: string) {
+    if(path.startsWith('/')) {
+      return this.root.fsPath;
+    }
+
+    return this.base.fsPath;
+  }
+
+  static isDir(path: string) {
+    return path.endsWith('/');
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -3,22 +3,28 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import FileManager from './FileManager';
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "better-new-file" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    let disposable = vscode.commands.registerCommand('extension.simpleNewFile', async () => {
+        if (!vscode.window.activeTextEditor) {
+            return;  // no file open
+        }
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        let basePath = vscode.window.activeTextEditor.document.uri;
+        let fileManager = new FileManager(basePath);
+
+        let path = await vscode.window.showInputBox();
+        let newFilePath = await fileManager.create(path);
+        if(!FileManager.isDir(newFilePath)) {
+            let doc = await vscode.workspace.openTextDocument(newFilePath);
+            vscode.window.showTextDocument(doc);
+        }
     });
 
     context.subscriptions.push(disposable);
