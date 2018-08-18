533e873277a572a949b57026b12f7a5efc6ee730
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2018-08-18 21:20:27 +0200

    It is nor possible to get the stats for a given file.

--- src/FileManager.ts
+++ src/FileManager.ts
@@ -1,14 +1,43 @@
-import { Uri, workspace } from 'vscode';
+import * as vscode from 'vscode';
 import * as mkdirp from 'mkdirp';
 import * as path from 'path';
 import * as fs from 'fs';
 
-export default class FileManager {
-  private base: Uri;
-  private root: Uri = workspace.workspaceFolders[0].uri;
+import FileStat from './FileStat';
 
-  constructor(base: Uri) {
-    this.base = Uri.file(path.dirname(base.fsPath));
+function handleResult<T>(resolve: (result: T) => void, reject: (error: Error) => void, error: Error | null | undefined, result: T): void {
+  if (error) {
+      reject(massageError(error));
+  } else {
+      resolve(result);
+  }
+}
+
+function massageError(error: Error & { code?: string }): Error {
+  if (error.code === 'ENOENT') {
+      return vscode.FileSystemError.FileNotFound();
+  }
+
+  if (error.code === 'EISDIR') {
+      return vscode.FileSystemError.FileIsADirectory();
+  }
+
+  if (error.code === 'EEXIST') {
+      return vscode.FileSystemError.FileExists();
+  }
+
+  if (error.code === 'EPERM' || error.code === 'EACCESS') {
+      return vscode.FileSystemError.NoPermissions();
+  }
+
+  return error;
+}
+
+export default class FileManager {
+  private root: vscode.WorkspaceFolder | undefined;
+
+  constructor(private base: vscode.Uri) {
+    this.root = vscode.workspace.getWorkspaceFolder(base);
   }
 
   create(pPath: string): Promise<string> {
@@ -40,7 +69,7 @@ export default class FileManager {
 
   getBase(path: string = undefined) {
     if(path !== undefined && path.startsWith('/')) {
-      return this.root.fsPath;
+      return this.root.uri.fsPath;
     }
 
     return this.base.fsPath;
@@ -48,5 +77,19 @@ export default class FileManager {
 
   static isDir(path: string) {
     return path.endsWith('/');
+  }
+
+  static stat(uri: vscode.Uri): Thenable<vscode.FileStat> {
+    return FileManager._stat(uri.fsPath);
+  }
+
+  private static async _stat(path: string): Promise<vscode.FileStat> {
+    return new FileStat(await FileManager.getFsStat(path));
+  }
+
+  private static getFsStat(path: string): Promise<fs.Stats> {
+    return new Promise<fs.Stats>((resolve, reject) => {
+        fs.stat(path, (error, stat) => handleResult(resolve, reject, error, stat));
+    });
   }
 }
---
+++ src/FileStat.ts
@@ -0,0 +1,35 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+export default class FileStat implements vscode.FileStat {
+
+  constructor(private fsStat: fs.Stats) {}
+
+  get type(): vscode.FileType {
+      return this.fsStat.isFile() ? vscode.FileType.File : this.fsStat.isDirectory() ? vscode.FileType.Directory : this.fsStat.isSymbolicLink() ? vscode.FileType.SymbolicLink : vscode.FileType.Unknown;
+  }
+
+  get isFile(): boolean | undefined {
+      return this.fsStat.isFile();
+  }
+
+  get isDirectory(): boolean | undefined {
+      return this.fsStat.isDirectory();
+  }
+
+  get isSymbolicLink(): boolean | undefined {
+      return this.fsStat.isSymbolicLink();
+  }
+
+  get size(): number {
+      return this.fsStat.size;
+  }
+
+  get ctime(): number {
+      return this.fsStat.ctime.getTime();
+  }
+
+  get mtime(): number {
+      return this.fsStat.mtime.getTime();
+  }
+}
